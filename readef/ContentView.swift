6abe134bbc78759fe7acbd3f6f6ff493f716bd6e
chiruuu21 <160108724+chiruuu21@users.noreply.github.com>
2025-12-03 13:39:49 -0600

    Initial Commit

---
+++ readef/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  readef
+//
+//  Created by Chirag Dhungana on 12/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ readef/readefApp.swift
@@ -0,0 +1,17 @@
+//
+//  readefApp.swift
+//  readef
+//
+//  Created by Chirag Dhungana on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct readefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
